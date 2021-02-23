2fd9bea352021d0f63af73e3664fb677a9e4d796
Samuel Goto <samuelgoto@gmail.com>
2021-02-22 21:54:07 -0800

    Set up mocha and a basic grammar generator

--- tests/parser.js
+++ tests/parser.js
@@ -1,7 +1,35 @@
 const assert = require("assert");
+const nearley = require("nearley");
+const compile = require("nearley/lib/compile");
+const generate = require("nearley/lib/generate");
+const nearleyGrammar = require("nearley/lib/nearley-language-bootstrapped");
 
 describe("Parser", function() {
+
+  function build(sourceCode) {
+    // Parse the grammar source into an AST
+    const grammarParser = new nearley.Parser(nearleyGrammar);
+    grammarParser.feed(sourceCode);
+    const grammarAst = grammarParser.results[0]; // TODO check for errors
+
+    // Compile the AST into a set of rules
+    const grammarInfoObject = compile(grammarAst, {});
+    // Generate JavaScript code from the rules
+    const grammarJs = generate(grammarInfoObject, "grammar");
+
+    // Pretend this is a CommonJS environment to catch exports from the grammar.
+    const module = { exports: {} };
+    eval(grammarJs);
+
+    return module.exports;
+  }
+
   it("Basic", function() {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+    const grammar = build(`
+      main -> "foo" | bar
+    `);
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+    const {results} = parser.feed("foo");
+    assert.deepEqual(results, [["foo"]]);
   });
 });
