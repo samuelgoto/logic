9ae3ec1ac53100aebfb50359728ea0fab3ef8f75
Samuel Goto <samuelgoto@gmail.com>
2021-03-18 21:13:46 -0700

    Start playing with one of the prolog solvers

--- tests/solver.js
+++ tests/solver.js
@@ -1,8 +1,70 @@
 const assert = require("assert");
 const {Solver, atMostOne, or} = require("logic-solver");
-
+const pl = require( "tau-prolog" );
 
 describe("Solver", function() {
+
+  class Interpreter {
+    constructor() {
+      this.session = pl.create();
+    }
+    consult(code) {
+      const session = this.session;
+      return new Promise((resolve, reject) => {
+        session.consult(code, {
+          success: resolve,
+          error: reject
+        });
+      });
+    }
+    query(code) {
+      const session = this.session;
+      return new Promise((resolve, reject) => {
+        session.query(code, {
+          success: resolve,
+          error: reject
+        });
+      });
+    }
+    async *answer(code) {
+      const session = this.session;
+      const go = () => new Promise((resolve, reject) => {
+        session.answer({
+          success: (answer) => resolve(answer),
+          fail: () => resolve(false),
+          error: reject,
+          limit: reject,
+        });
+      });
+
+      do {
+        const answer = await go();
+        if (!answer) {
+          return;
+        }
+        yield answer;
+      } while (true);
+    }
+  }
+  
+  it("tau", async () => {
+    const prolog = new Interpreter();
+    await prolog.consult(`
+      likes(sam, salad).
+      likes(dean, pie).
+      likes(sam, apples).
+      likes(dean, whiskey).
+    `);
+    await prolog.query(`likes(sam, X).`);
+    const answer = await prolog.answer();
+    let result = [];
+    for await (let {links} of prolog.answer()) {
+      const keypairs = Object.entries(links).map(([key, value]) => [key, value.id]);
+      result.push(Object.fromEntries(keypairs));
+    }
+    assertThat(result).equalsTo([{X: "salad"}, {X: "apples"}]);
+  });
+  
   it("basic", function() {
     // console.log(Solver);
     const solver = new Solver();
